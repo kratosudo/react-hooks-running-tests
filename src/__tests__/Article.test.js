ec64faf09e215c1a250dc3f1c4c18e48748f3c65
kibiruki <+>
2024-07-30 05:18:18 +0300

    complete lab

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -7,5 +7,19 @@ import Article from "../components/Article";
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  //   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  // });
+  //   //to se how the DOM looks like
+  //   screen.debug();
+
+  //   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  // });
+ 
+
+  //to debug a single element
+  const element = screen.queryByText("please pass this test");
+
+  screen.debug(element);
+
+  expect(element).toBeInTheDocument();
 });
